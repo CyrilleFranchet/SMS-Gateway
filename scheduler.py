aeb3cc083ba314e2de8a3b3b78852847849e4c17
CyrilleFranchet <cf.poubelle@gmail.com>
2015-08-25 15:44:52 +0200

    dev

--- scheduler.py
+++ scheduler.py
@@ -12,7 +12,7 @@ class Scheduler(threading.Thread):
     def __init__(self, fifo):
         self.debug = debug
         self.stop = threading.Event()
-        self.dict_jobs = OrderedDict()
+        self.dict_jobs_to_procede = OrderedDict()
         self.id = 0
         self.fifo = fifo
         self.lock = threading.Lock()
@@ -25,7 +25,7 @@ class Scheduler(threading.Thread):
         self.id += 1
         temp_id = self.id
         self.lock.release()
-        self.dict_jobs[temp_id] = new_sms
+        self.dict_jobs_top_rocede[temp_id] = new_sms
         return temp_id
 
     def SchedulerGetJob(self, username, id):
@@ -34,9 +34,13 @@ class Scheduler(threading.Thread):
     def run(self):
         while not self.stop.isSet():
             while self.dict_jobs:
-                print 'scheduler list', self.dict_jobs
-                _, sms = self.dict_jobs.popitem(last=False)
+                _, sms = self.dict_jobs_to_procede.popitem(last=False)
+                if self.debug: print 'Sending SMS into FIFO', sms
                 self.fifo.put(sms, True, 5)
                 self.fifo.join()
-                modem_response = sms.queue.get(True, 5)
+                try:
+                    modem_response = sms.queue.get(True, 15)
+                except Queue.Empty:
+                    pass
+                print 'Scheduler received modem response :', modem_response
                 sms.queue.task_done()
